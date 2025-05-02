0b7d3733a84252a734d7948c2087b315bfd396a7
Joni Roine <joni.roine@outlook.com>
2025-05-02 14:11:57 +0300

    Add initial setup for logistic regression model with necessary imports

---
+++ machine_learning/lab3_logistic_regression/logistic_regression.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.metrics import log_loss
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')
