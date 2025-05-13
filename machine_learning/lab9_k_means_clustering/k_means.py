967f8ae0829c1155199e5f12787a3723ba509f06
Joni Roine <joni.roine@outlook.com>
2025-05-13 14:48:01 +0300

    Refactor K-means clustering script: streamline K=3 clustering implementation, update plotting logic, and enhance code readability

--- machine_learning/lab9_k_means_clustering/k_means.py
+++ machine_learning/lab9_k_means_clustering/k_means.py
@@ -23,53 +23,51 @@ plt.title('Original Data Points')
 plt.xticks(())
 plt.yticks(())
 
-# Compare K=4 vs K=3 clustering
-# Create a figure with two subplots side by side
-plt.figure(figsize=(14, 6))
+# K=3 clustering
+# Create a figure for the K-means plot
+plt.figure(figsize=(10, 6))
 
-# K=4 clustering (original)
-# -----------------------
-# Initialize K-means model with 4 clusters
-k_means_4 = KMeans(init="k-means++", n_clusters=4, n_init=12, random_state=0)
+# Initialize K-means model with 3 clusters
+k_means_3 = KMeans(init="k-means++", n_clusters=3, n_init=12, random_state=0)
 
 # Fit the model to the data
-k_means_4.fit(X)
-k_means_labels_4 = k_means_4.labels_
-k_means_cluster_centers_4 = k_means_4.cluster_centers_
+k_means_3.fit(X)
+k_means_labels_3 = k_means_3.labels_
+k_means_cluster_centers_3 = k_means_3.cluster_centers_
 
-# Create the first subplot for K=4
-ax1 = plt.subplot(1, 2, 1)
+# Create the plot
+ax1 = plt.subplot(1, 1, 1)
 
 # Generate colors based on the number of clusters
-colors_4 = plt.cm.tab10(np.linspace(0, 1, len(set(k_means_labels_4))))
+colors_3 = plt.cm.tab10(np.linspace(0, 1, len(set(k_means_labels_3))))
 
 # For loop that plots the data points and centroids.
-# k will range from 0-3, which will match the possible clusters that each
+# k will range from 0-2, which will match the possible clusters that each
 # data point is in.
-for k, col in zip(range(len([[4, 4], [-2, -1], [2, -3], [1, 1]])), colors):
+for k, col in zip(range(len(set(k_means_labels_3))), colors_3):
 
     # Create a list of all data points, where the data points that are 
     # in the cluster (ex. cluster 0) are labeled as true, else they are
     # labeled as false.
-    my_members = (k_means_labels == k)
+    my_members = (k_means_labels_3 == k)
 
     # Define the centroid, or cluster center.
-    cluster_center = k_means_cluster_centers[k]
+    cluster_center = k_means_cluster_centers_3[k]
 
     # Plots the datapoints with color col.
-    ax.plot(X[my_members, 0], X[my_members, 1], 'w', markerfacecolor=col, marker='.',ms=10)
+    ax1.plot(X[my_members, 0], X[my_members, 1], 'w', markerfacecolor=col, marker='.',ms=10)
 
     # Plots the centroids with specified color, but with a darker outline
-    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,  markeredgecolor='k', markersize=6)
+    ax1.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,  markeredgecolor='k', markersize=6)
 
 # Title of the plot
-ax.set_title('KMeans')
+ax1.set_title('KMeans with 3 Clusters')
 
 # Remove x-axis ticks
-ax.set_xticks(())
+ax1.set_xticks(())
 
 # Remove y-axis ticks
-ax.set_yticks(())
+ax1.set_yticks(())
 
 # Show the plot
 plt.show()
